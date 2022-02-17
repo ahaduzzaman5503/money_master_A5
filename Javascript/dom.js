f2c909b5dc0815fbd001c4853be2446fb88df43e
ahaduzzaman550 <ahaduzzaman5503@gmail.com>
2022-02-17 23:31:59 +0600

    Complete Error Handling

--- Javascript/dom.js
+++ Javascript/dom.js
@@ -1,39 +1,52 @@
+// Income Calculation Function Start ==========
 function incomeCalculate(){
+    // Food =====
+
     let food = document.getElementById("food-input");
     let foodValue = food.value;
     let foodIntValue = parseFloat(foodValue);
-   
+
+    // Rent =====
     let rent = document.getElementById("rent-input");
     let rentValue = rent.value
     let rentIntValue = parseFloat(rentValue);
 
+    // Clothes =====
     let clothes = document.getElementById("cloth-input");
     let clothesValue = clothes.value;
     let clothIntValue = parseFloat(clothesValue);
-   
-    let total = (foodIntValue + rentIntValue + clothIntValue);
-    let totalValue =  total;
-    
-    let totalExpenses = document.getElementById("total-expenses");
-    let totalCost = totalExpenses.innerText = totalValue;
 
-    /* ================================================================= */
+    // Call Income inful field =========== 
     let incomeInputField = document.getElementById("income-input");
     let incomeInputValue = incomeInputField.value;
     let incomeValueNum = parseFloat(incomeInputValue);
 
+    // Error Handling ===========
+    if ( foodIntValue > 0 && rentIntValue > 0 && clothIntValue > 0 && incomeInputValue > 0 ) {
+        let total = (foodIntValue + rentIntValue + clothIntValue);
+        let totalValue =  total;
+        // Call Total Expenses ===================
+        let totalExpenses = document.getElementById("total-expenses");
+        let totalCost = totalExpenses.innerText = totalValue;
+       
+        let remaining = incomeValueNum - total;
 
-     let remaining = incomeValueNum - total;
+         // Call TotalBalance field =========== 
+        let TotalBalance = document.getElementById("totalBalance");
+        let BalanceText = TotalBalance.innerText;
+        let remainingBalance =  TotalBalance.innerText = remaining;
+    } else {
+        alert("Please Enter Valid number")
+    }
 
-    let TotalBalance = document.getElementById("totalBalance");
-    let BalanceText = TotalBalance.innerText;
-    let remainingBalance =  TotalBalance.innerText = remaining;
- food.value= " ";
- rent.value= " ";
- clothes.value= " ";
- incomeInputField.value= " ";
+    // Emty all input field
+    food.value= " ";
+    rent.value= " ";
+    clothes.value= " ";
+    incomeInputField.value= " ";
 }
 
+// Saving Calculate Function Start =================
 function savingCalculate() {
     // Saving Amount
     let savingAmount = document.getElementById("saving-amount");
@@ -52,29 +65,16 @@ function savingCalculate() {
     console.log(parsantCalculate);
     totalSavingAmount = savingAmount.innerText = parsantCalculate;
 
-
-   let finalRemainingBalance = BalanceTextNumber - totalSavingAmount;
-   console.log( "This is final amount" + finalRemainingBalance);
+    // Final Balance Calculate ==================
+    let finalRemainingBalance = BalanceTextNumber - totalSavingAmount;
 
     let remainingBalance = document.getElementById("remaining-balance");
     let remainingBalanceValue = remainingBalance.innerText;
-    remainingBalance.innerText = finalRemainingBalance;
-    // let remainingBalanceValueNumber = parseFloat(remainingBalanceValue);
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let finalBlance = remainingBalance.innerText = finalRemainingBalance;
+    //  Call Remaining Balance ==================
+    let remainingBalanceValueNumber = parseFloat(finalBlance);
+    // Clear Save Input field =====================
     saveParsant.value = " ";
     income.value = " ";
-
-
 }
+
